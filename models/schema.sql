ed477b119a7376a7c29785d8e301679372ad1ca3
dustin george <DustinG27@gmail.com>
2020-05-18 17:46:22 -0400

    connection.js

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS budgetBuddy_db;
 CREATE DATABASE budgetBuddy_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+-- DROP DATABASE IF EXISTS testdb; --
+-- CREATE DATABASE testdb; --
