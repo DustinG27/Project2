a0528faf67b790eb10a3a40a78cc8ed5cc656a69
dustin george <DustinG27@gmail.com>
2020-05-18 20:41:49 -0400

    test4

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    // Database is JawsDB on Heroku
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  // Database is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    // Database is local
-    connection = mysql.createConnection({
-        port: 3000,
-        host: 'localhost',
-        user: 'root',
-        password: 'Weezer#123',
-        database: 'budgetBuddy_db'
-    })
-};
+  // Database is local
+  connection = mysql.createConnection({
+    port: 3000,
+    host: "localhost",
+    user: "root",
+    password: "Weezer#123",
+    database: "budgetBuddy_db"
+  });
+}
 
 connection.connect();
 module.exports = connection;
